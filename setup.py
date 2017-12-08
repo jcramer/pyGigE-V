a8f530b3668bf24b507190c8f7b662168c2a9943
J. Cramer <jscramer@gmail.com>
2017-12-08 14:32:50 -0500

    renaming

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension("pygev", 
-                             ["pygev.pyx"], 
+ext_modules = [Extension("pygigev", 
+                             ["pygigev.pyx"], 
                              language="c",
                              include_dirs=["/usr/dalsa/GigeV/include/"],
                              libraries=["GevApi"],
                              )]
 
 setup(
-    name='pygev',
+    name='pygigev',
     cmdclass = {'build_ext': build_ext},
     ext_modules=ext_modules
     )
