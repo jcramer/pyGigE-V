8b9df33623ec831c41c04bac868175580b605965
J. Cramer <jscramer@gmail.com>
2017-12-13 11:54:53 -0500

    updates

--- pygigev.pyx
+++ pygigev.pyx
@@ -1,63 +1,76 @@
 import numpy as np
+
 cimport numpy as np
-
 from cython cimport view
-from cython.view cimport array as cvarray
-
 from libc.stdlib cimport malloc, free
 
 cimport decl
 
 cdef class PyGigEV:
-	cdef:
-		decl.GEV_CAMERA_INFO cameras
-		decl.GEV_CAMERA_HANDLE handle
-		decl.GEV_BUFFER_OBJECT* image_object_ptr
-		decl.UINT8[:, ::1] buffers
-		decl.UINT8** buffers_ptr
+	cdef decl.GEV_CAMERA_INFO cameras
+	cdef decl.GEV_CAMERA_HANDLE handle
+	cdef decl.GEV_BUFFER_OBJECT* image_object_ptr
+	cdef decl.UINT8[:, ::1] buffers
+	cdef decl.UINT8** buffers_ptr
+	cdef decl.UINT32 width
+	cdef decl.UINT32 height
+	cdef decl.UINT32 x_offset
+	cdef decl.UINT32 y_offset
+	cdef decl.UINT32 format
 
 	def __cinit__(self):
 		self.handle = NULL
+		self.width = 0
+		self.height = 0
+		self.x_offset = 0
+		self.y_offset = 0
+		self.format = 0
 
 	def __init__(self):
 		self.GevGetCameraList()
 
 	def GevGetCameraList(self, int maxCameras=1000):
 		cdef int numCameras
-		cdef decl.GEV_STATUS exitcode = decl.GevGetCameraList(&self.cameras, maxCameras, &numCameras)
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevGetCameraList(&self.cameras, maxCameras, &numCameras)
 		return (self.handleExitCode(exitcode), self.cameras, numCameras)
 
 	def GevOpenCamera(self, int gevAccessMode=4, int cameraListIndex=0):
 		cdef decl.GEV_CAMERA_INFO _device = self.cameras  # what happens with multiple cameras in list??
-		cdef decl.GEV_STATUS exitcode = decl.GevOpenCamera(&_device, <decl.GevAccessMode>gevAccessMode, &self.handle)
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevOpenCamera(&_device, <decl.GevAccessMode>gevAccessMode, &self.handle)
 		return self.handleExitCode(exitcode)
 
 	def GevCloseCamera(self):
-		cdef decl.GEV_STATUS exitcode = decl.GevCloseCamera(&self.handle)
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevCloseCamera(&self.handle)
 		free(self.buffers_ptr)
 		return self.handleExitCode(exitcode)
 
 	def GevGetCameraInterfaceOptions(self):
 		cdef decl.GEV_CAMERA_OPTIONS options
-		cdef decl.GEV_STATUS exitcode = decl.GevGetCameraInterfaceOptions(self.handle, &options)
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevGetCameraInterfaceOptions(self.handle, &options)
 		return (self.handleExitCode(exitcode), options)
 
 	def GevSetCameraInterfaceOptions(self, options):
 		cdef decl.GEV_CAMERA_OPTIONS _options = options
-		cdef decl.GEV_STATUS exitcode = decl.GevSetCameraInterfaceOptions(self.handle, &_options)
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevSetCameraInterfaceOptions(self.handle, &_options)
 		return self.handleExitCode(exitcode)
 
 	def GevGetImageParameters(self):
-		cdef decl.UINT32 width = 0
-		cdef decl.UINT32 height = 0
-		cdef decl.UINT32 x_offset = 0
-		cdef decl.UINT32 y_offset = 0
-		cdef decl.UINT32 format = 0
-		cdef decl.GEV_STATUS exitcode = decl.GevGetImageParameters(self.handle, &width, &height, &x_offset, &y_offset, &format)
-		return {'code': exitcode, 'width': width, 'height': height, 'x_offset': x_offset, 'y_offset': y_offset, 'pixelFormat':(format, hex(format))}
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevGetImageParameters(self.handle, &self.width, &self.height, &self.x_offset, &self.y_offset, &self.format)
+		return {'code': exitcode, 'width': self.width, 'height': self.height, 'x_offset': self.x_offset, 'y_offset': self.y_offset, 'pixelFormat':(self.format, hex(self.format))}
+
+	def GevSetImageParameters(self, decl.UINT32 width, decl.UINT32 height, decl.UINT32 x_offset, decl.UINT32 y_offset, decl.UINT32 format):
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevSetImageParameters(self.handle, width, height, x_offset, y_offset, format)
+		return self.handleExitCode(exitcode)
 
 	def GevInitImageTransfer(self, int bufferCyclingMode=1, int numImgBuffers=8):
-		cdef decl.GEV_STATUS exitcode
+		cdef decl.GEV_STATUS exitcode = 0
 		imgParams = self.GevGetImageParameters()
 		cdef decl.UINT32 size = self.GetPixelSizeInBytes(imgParams['pixelFormat'][0]) * \
 								imgParams['width'] * imgParams['height']
@@ -83,14 +96,14 @@ cdef class PyGigEV:
 		return self.handleExitCode(exitcode)
 
 	def GevInitializeImageTransfer(self, int numImgBuffers=8):
-		cdef decl.GEV_STATUS exitcode
+		cdef decl.GEV_STATUS exitcode = 0
 		imgParams = self.GevGetImageParameters()
 		cdef decl.UINT32 size = self.GetPixelSizeInBytes(imgParams['pixelFormat'][0]) * \
 								imgParams['width'] * imgParams['height']
 
 		# create variable to hold a image buffer
 		self.buffers = np.empty(shape=[numImgBuffers,size], dtype=np.uint8, order="C")
-		
+
 		# create helper array to store image array pointers
 		self.buffers_ptr = <decl.UINT8**>malloc(numImgBuffers * sizeof(decl.UINT8*))
 
@@ -100,7 +113,7 @@ cdef class PyGigEV:
 			for i in range(numImgBuffers):
 				self.buffers_ptr[i] = &self.buffers[i,0]
 
-			exitcode = decl.GevInitializeImageTransfer(self.handle, numImgBuffers, &self.buffers_ptr[0])
+			exitcode = decl.GevInitializeImageTransfer(self.handle, numImgBuffers, self.buffers_ptr)
 		except:
 			pass
 		#finally:
@@ -109,40 +122,85 @@ cdef class PyGigEV:
 		return self.handleExitCode(exitcode)
 
 	def GevStartImageTransfer(self, int numFrames):
-		cdef decl.GEV_STATUS exitcode = decl.GevStartImageTransfer(self.handle, <decl.UINT32>numFrames)
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevStartImageTransfer(self.handle, <decl.UINT32>numFrames)
 		return self.handleExitCode(exitcode)
 
+	# partially working, can return an image in the buffer, but not properly through API
 	def GevGetImageBuffer(self):
 		return np.asarray(self.buffers[0,:])
-		#cdef void** buffer_ptr
-		#cdef decl.GEV_STATUS exitcode
-		#exitcode = decl.GevGetImageBuffer(self.handle, buffer_ptr)
+		# cdef int size = self.height * self.width
+		# cdef decl.UINT8** _buffer = NULL
+		# cdef decl.GEV_STATUS exitcode = 0
+		# exitcode = decl.GevGetImageBuffer(self.handle, <void**>_buffer)
+		# cdef view.array buffer_view = view.array(shape=(size,), itemsize=sizeof(decl.UINT8), format="i", mode="c", allocate_buffer=False)
+		# buffer_view.data = <char *> _buffer
 
-		##cdef view.array buffer_view = <np.uint8[:10]> buffer_ptr
-		#cdef view.array buffer_view = view.array(shape=(100), itemsize=sizeof(decl.UINT8), format="i", mode="c", allocate_buffer=False)
-		#buffer_view.data = <char *> buffer_ptr
+		# print "exitcode: "
+		# print exitcode
 
-		#return exitcode
+		# print "printing _buffer value"
+		# print <unsigned int>_buffer
+		# print "{0:x}".format(<unsigned int>_buffer)
 
-	# def GevWaitForNextImageBuffer(self, int timeout):
+		# print "printing handle value"
+		# print "{0:x}".format(self.handle)
+		# print "{0:x}".format(<unsigned int>&self.handle)
+
+		# print "buffers addresses"
+		# for i in range(8):
+		# 	print "{0:x}".format(<unsigned int>&self.buffers[i,0])
+
+		# print "buffers_ptr addresses"
+		# for i in range(8):
+		# 	print "{0:x}".format(<unsigned int>self.buffers_ptr[i])
+
+		# print "returning as numpy array"
+		# return np.asarray(buffer_view)
+
+	# def GevWaitForNextImageBuffer(self, decl.UINT32 timeout=1000):
 	# 	cdef void** buffer_ptr = NULL
 	# 	cdef decl.GEV_STATUS exitcode
 	# 	exitcode = decl.GevWaitForNextImageBuffer(self.handle, buffer_ptr, <decl.UINT32>timeout)
 	# 	return exitcode
 
-	# not working?
+	# not working, struct returns weird values
+	def GevWaitForNextImage(self, decl.UINT32 timeout=1000):
+		cdef decl.GEV_BUFFER_OBJECT* img
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevWaitForNextImage(self.handle, &img, timeout)
+
+		if img is NULL:
+			return "NULL"
+
+		print "buffer object"
+		print "{0:x}".format(<unsigned int>&img)
+		print "{0:x}".format(img.status)
+		print img.status
+		print img.state
+		print img.w
+		print img.h
+		
+		print "exitcode: " + str(exitcode)
+
+		return str(img.status)
+
+	# not working
 	def GevStopImageTransfer(self):
-		cdef decl.GEV_STATUS exitcode = decl.GevStopImageTransfer(self.handle)
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevStopImageTransfer(self.handle)
 		return self.handleExitCode(exitcode)
 
-	# not working?
+	# not working
 	def GevAbortImageTransfer(self):
-		cdef decl.GEV_STATUS exitcode = decl.GevAbortImageTransfer(self.handle)
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevAbortImageTransfer(self.handle)
 		return self.handleExitCode(exitcode)
 	
 	# havn't tested since previous 2 aren't working
-	def GevReleaseImage(self):
-		cdef decl.GEV_STATUS exitcode = decl.GevReleaseImage(&self.handle, self.image_object_ptr)
+	def GevReleaseImageBuffer(self):
+		cdef decl.GEV_STATUS exitcode = 0
+		exitcode = decl.GevReleaseImageBuffer(self.handle, &self.buffers_ptr[0])
 		return self.handleExitCode(exitcode)
 
 	@staticmethod
@@ -156,14 +214,14 @@ cdef class PyGigEV:
 	@staticmethod
 	def GevGetLibraryConfigOptions():
 		cdef decl.GEVLIB_CONFIG_OPTIONS options
-		cdef decl.GEV_STATUS exitcode
+		cdef decl.GEV_STATUS exitcode = 0
 		exitcode = decl.GevGetLibraryConfigOptions(&options)
 		return (exitcode, options)
 
 	@staticmethod
 	def GevSetLibraryConfigOptions(object options):
 		cdef decl.GEVLIB_CONFIG_OPTIONS _options = options
-		cdef decl.GEV_STATUS exitcode
+		cdef decl.GEV_STATUS exitcode = 0
 		exitcode = decl.GevGetLibraryConfigOptions(&_options)
 		return exitcode
 
@@ -184,4 +242,3 @@ cdef class PyGigEV:
 		if exitcode is not 0:
 			return "Method returned code " + str(exitcode) + ", please check your camera's manual."
 		else: return "OK"
-
--- test.py
+++ test.py
@@ -1,35 +1,72 @@
 from pygigev import PyGigEV as gev
+import timeit
 import cv2
 
 # create new context to store native camera data
-cxt = gev()
+ctx = gev()
+
+# print list of available cameras
+print ctx.GevGetCameraList()
 
 # open the first detected camera - returns 'OK'
-cxt.GevOpenCamera()
+ctx.GevOpenCamera()
 
 # get image parameters - returns python object of params
-params = cxt.GevGetImageParameters()
+params = ctx.GevGetImageParameters()
+print "Initial image parameters:"
+print params
+
+# camera sensor properties
+width_max = 1936
+height_max = 1216
+binning = 0
+saturation = 0
+brightness = 0
+contrast = 0
+
+# desired properties
+crop_factor = 1.0
+width = int(width_max * 1/crop_factor)
+height = int(height_max * 1/crop_factor)
+x_offset = int((width_max - width) / 2)
+y_offset = int((height_max - height) / 2)
+
+ctx.GevSetImageParameters(width, height, x_offset, y_offset, params['pixelFormat'][0])
+params = ctx.GevGetImageParameters()
+print "Final image parameters:"
+print params
+
 width = params['width']
 height = params['height'] 
 
 # allocate image buffers and prepare for async image transfer to buffer
-cxt.GevInitializeImageTransfer(2)
+ctx.GevInitializeImageTransfer(1)
 
 # start transfering images to memory buffer, use -1 for streaming or [1-9] for num frames 
-cxt.GevStartImageTransfer(-1)
+ctx.GevStartImageTransfer(-1)
 
 while(True):
     # simply return numpy array for first image in buffer
-    img = cxt.GevGetImageBuffer().reshape(height, width) # is there a more efficient way to reshape?
+    img = ctx.GevGetImageBuffer().reshape(height, width) # is there a more efficient way to reshape?
+    if width > 600 or height > 600:
+        img = cv2.resize(img, (int(width*0.25),int(height*0.25)))
     cv2.imshow('pyGigE-V', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
 cv2.destroyAllWindows()
 
 # Stop transfer, release memory, close camera connection 
-cxt.GevStopImageTransfer()  # not working at the moment
-cxt.GevAbortImageTransfer() # not working at the moment
-cxt.GevReleaseImage()       # not working at the moment, will need to exit python to release memory
-cxt.GevCloseCamera()
+#ctx.GevStopImageTransfer()  # not working at the moment
+#ctx.GevAbortImageTransfer() # not working at the moment
+#ctx.GevReleaseImageBuffer() # not working at the moment, will need to exit python to release memory
+ctx.GevCloseCamera()
+
+
+# def reshape(height, width):
+#     img = ctx.GevGetImageBuffer()
+#     return img.reshape(height, width)
+
+# timeit.timeit(stmt='reshape(height, width)', setup="from __main__ import reshape, width, height", number=100000)
+# timeit.timeit(stmt='ctx.GevGetImageBufferReshaped(height, width)', setup="from __main__ import ctx, width, height", number=100000)
+#timeit -r15 ctx.GevGetImageBufferResize(height, width)
